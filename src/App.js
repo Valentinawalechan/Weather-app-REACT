68c34a8e37c28e3277fe67fb020a4cdd401fe28b
Valentinawalechan <vale.disa86@gmail.com>
2021-05-24 17:17:53 +0100

    added github link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1> Weather App </h1>
+      <footer>
+        This project was coded by Valentina Di Santo and is 
+      <a href="https://github.com/Valentinawalechan/Weather-app-REACT" target="_blank"> open-sourced on GitHub </a>
+    </footer>
     </div>
+
   );
 }
 
