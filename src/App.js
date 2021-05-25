516e5217989d7fa5455d2d92ccb8005880c933fe
Valentinawalechan <vale.disa86@gmail.com>
2021-05-25 13:08:20 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1> Weather App </h1>
+     <button className="btn btn-primary"> hello </button>
       <footer>
         This project was coded by Valentina Di Santo and is 
       <a href="https://github.com/Valentinawalechan/Weather-app-REACT" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
     </footer>
+    </div>
     </div>
 
   );
