dac08a5f55010b45eb5702e8ec4b13c2fe7d08d6
Valentinawalechan <vale.disa86@gmail.com>
2021-05-25 16:17:26 +0100

    added weather

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import './App.css';
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1> Weather App </h1>
-     <button className="btn btn-primary"> hello </button>
+       <Weather />
       <footer>
         This project was coded by Valentina Di Santo and is 
       <a href="https://github.com/Valentinawalechan/Weather-app-REACT" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
